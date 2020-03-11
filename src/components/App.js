6974396cd4752e2656101b470c88edac1b38fd15
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-12 02:28:45 +0300

    Add data retriever & hooks effects

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
+import ImageList from "./ImageList";
+import PageButtons from "./PageButtons";
+import ErrorBox from "./ErrorBox";
+import imageGetter from "../api/imageGetter";
 import "./App.css";
 
 const App = () => {
@@ -11,6 +15,42 @@ const App = () => {
   const [totalPages, setTotalPages] = useState(1);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const onSearchSubmit = async () => {
+    setError("");
+    setLoading(true);
+
+    const [{ results, total_pages }, e] = await imageGetter(
+      term,
+      collection,
+      page,
+      setError
+    );
+    if (e) {
+      setError(e);
+    } else {
+      setImages(results);
+      setTotalPages(total_pages);
+      if (results.length === 0) setError("No result has been found");
+    }
+
+    setLoading(false);
+  };
+
+  // triggers on form submit
+  useEffect(() => {
+    if (page === 1) {
+      // when already in first page, setting page does not trigger second useEffect
+      onSearchSubmit();
+    } else {
+      setPage(1); // this triggers the other useEffect's onSearchSubmit
+    }
+  }, [term, collection]);
+
+  //trigger on page change
+  useEffect(() => {
+    onSearchSubmit();
+  }, [page]);
 
   return (
     <div className="App">
