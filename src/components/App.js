154b3db2e2dfad65ff2cb9c2854104720ac9bf69
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-12 02:14:35 +0300

    Add header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+import Header from "./Header";
 import "./App.css";
 
 const App = () => {
@@ -8,7 +9,13 @@ const App = () => {
   const [collection, setCollection] = useState(0);
   const [page, setPage] = useState(1);
 
-  return <div>Image Search App</div>;
+  return (
+    <div className="App">
+      <div className="navbar">
+        <Header setTerm={setTerm} setCollection={setCollection} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
