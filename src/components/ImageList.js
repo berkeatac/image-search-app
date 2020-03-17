47da3969a2ac129cb146faf60b9978d3ae6ffe28
berkeatac <berkeatac@sabanciuniv.edu>
2020-03-17 04:52:08 +0300

    Set keys to list elements & fix warnings

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -7,10 +7,12 @@ const ImageList = ({ images }) => {
   const items = images.map(img => {
     const height = img.height * (300 / img.width);
     return (
-      <div style={{ gridRowEnd: `span ${Math.ceil(height / 10) + 2}` }}>
-        <a href={img.links.html} target="_blank">
+      <div
+        style={{ gridRowEnd: `span ${Math.ceil(height / 10) + 2}` }}
+        key={img.id}
+      >
+        <a href={img.links.html} target="_blank" rel="noopener noreferrer">
           <img
-            key={img.id}
             src={img.urls.small}
             alt={img.alt_description}
             className="list-image"
