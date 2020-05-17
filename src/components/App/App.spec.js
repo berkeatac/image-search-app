6e00674ce2e992272da2434861aa1bd1bb21b3c0
berkeatac <berkeatac@sabanciuniv.edu>
2020-05-17 16:17:02 +0300

    Fix: Separate reducer and App tests

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -2,55 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 
 import App from "./index";
-import appReducer from "../../reducers/appReducer";
 
 describe("App", () => {
-  describe("Reducer", () => {
-    it("should set page with reducer", () => {
-      const state = {
-        images: [],
-        term: "istanbul",
-        collection: 0,
-        page: 1,
-        total_pages: 1,
-      };
-      const newState = appReducer(state, {
-        type: "SET_PAGE",
-        payload: { page: 2 },
-      });
-
-      expect(newState).toEqual({
-        images: [],
-        term: "istanbul",
-        collection: 0,
-        page: 2,
-        total_pages: 1,
-      });
-    });
-
-    it("should set page to 1 with new params", () => {
-      const state = {
-        images: [],
-        term: "istanbul",
-        collection: 0,
-        page: 5,
-        total_pages: 1,
-      };
-      const newState = appReducer(state, {
-        type: "SET_PARAMS",
-        payload: { term: "ankara", collection: 0 },
-      });
-
-      expect(newState).toEqual({
-        images: [],
-        term: "ankara",
-        collection: 0,
-        page: 1,
-        total_pages: 1,
-      });
-    });
-  });
-
   test("snapshot renders", () => {
     const component = renderer.create(<App />);
     let tree = component.toJSON();
---
+++ src/reducers/appReducer.spec.js
@@ -0,0 +1,47 @@
+import appReducer from "./appReducer";
+
+describe("Reducer", () => {
+  it("should set page with reducer", () => {
+    const state = {
+      images: [],
+      term: "istanbul",
+      collection: 0,
+      page: 1,
+      total_pages: 1,
+    };
+    const newState = appReducer(state, {
+      type: "SET_PAGE",
+      payload: { page: 2 },
+    });
+
+    expect(newState).toEqual({
+      images: [],
+      term: "istanbul",
+      collection: 0,
+      page: 2,
+      total_pages: 1,
+    });
+  });
+
+  it("should set page to 1 with new params", () => {
+    const state = {
+      images: [],
+      term: "istanbul",
+      collection: 0,
+      page: 5,
+      total_pages: 1,
+    };
+    const newState = appReducer(state, {
+      type: "SET_PARAMS",
+      payload: { term: "ankara", collection: 0 },
+    });
+
+    expect(newState).toEqual({
+      images: [],
+      term: "ankara",
+      collection: 0,
+      page: 1,
+      total_pages: 1,
+    });
+  });
+});
