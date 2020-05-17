7d72bc44dc5f7123a16be6a644354e844149b841
berkeatac <berkeatac@sabanciuniv.edu>
2020-05-17 16:10:32 +0300

    Feat: Add snapshot and unit test for reducer

---
+++ src/components/App/App.spec.js
@@ -0,0 +1,37 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "./index";
+import appReducer from "../../reducers/appReducer";
+
+describe("App", () => {
+  describe("Reducer", () => {
+    it("should set page with reducer", () => {
+      const state = {
+        images: [],
+        term: "istanbul",
+        collection: 0,
+        page: 1,
+        total_pages: 1,
+      };
+      const newState = appReducer(state, {
+        type: "SET_PAGE",
+        payload: { page: 2 },
+      });
+
+      expect(newState).toEqual({
+        images: [],
+        term: "istanbul",
+        collection: 0,
+        page: 2,
+        total_pages: 1,
+      });
+    });
+  });
+
+  test("snapshot renders", () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
