54e9066bff824a5b657c0889fd7440615d99508b
berkeatac <berkeatac@sabanciuniv.edu>
2020-09-29 14:37:11 +0300

    Updates

---
+++ precache-manifest.8ed05fe0f57b56110ed0697951c0bcbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c71bdfb0c1772e097ee35f68f49cb7",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "2af8cfb04ae78256e238",
+    "url": "/image-search-app/static/js/2.398cade9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-search-app/static/js/2.398cade9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a3e4d0332fd6122703",
+    "url": "/image-search-app/static/js/main.527c2f23.chunk.js"
+  },
+  {
+    "revision": "b955a792cf9e143abbff",
+    "url": "/image-search-app/static/js/runtime-main.d0e17b03.js"
+  },
+  {
+    "revision": "c17bde18661c8ef91b2b7a062782399f",
+    "url": "/image-search-app/static/media/dropdown_indicator.c17bde18.svg"
+  },
+  {
+    "revision": "19361864e4da1b88d614da2782cda941",
+    "url": "/image-search-app/static/media/logo.19361864.svg"
+  }
+]);
